d0522ed2a87b665a0a59ed2fca6e77c05d25b11e
Manu <gomanu6@gmail.com>
2020-11-27 20:54:19 +0530

    Adding the images for the products, making the layout somewhat responsive

--- src/components/productlist.js
+++ src/components/productlist.js
@@ -4,7 +4,7 @@ const productList = [
     description:
       "Earth is the third planet from the Sun and the only astronomical object known to harbor life.",
     price: 2000000,
-    image: "https://thumbs2.imgbox.com/42/f0/PxvkOJw9_t.jpg",
+    image: "https://www.flaticon.com/svg/static/icons/svg/744/744480.svg",
     pid: 1,
     category: "planets",
     quantity: 1,
@@ -15,7 +15,7 @@ const productList = [
     description:
       "The next destination after earth, currently being scouted by Mr Elon Musk for his future base for manufacturing spaceships",
     price: 800000000,
-    image: "https://thumbs2.imgbox.com/ce/1f/LlExbV6j_t.jpg",
+    image: "https://www.flaticon.com/svg/static/icons/svg/1086/1086102.svg",
     pid: 2,
     category: "planets",
     quantity: 1,
@@ -25,7 +25,7 @@ const productList = [
     description:
       "The former planet is a perfect place for second home with cool weather all year round.",
     price: 300000,
-    image: "https://thumbs2.imgbox.com/93/0f/LaqCH4qS_t.jpg",
+    image: "https://www.flaticon.com/svg/static/icons/svg/2909/2909930.svg",
     pid: 3,
     category: "planets",
     quantity: 1,
@@ -34,7 +34,7 @@ const productList = [
     title: "Spacesuit",
     description: "A must for humans, if you plan to travel in space.",
     price: 204500,
-    image: "https://thumbs2.imgbox.com/64/5f/M6qPUDtt_t.jpg",
+    image: "https://www.flaticon.com/svg/static/icons/svg/2531/2531029.svg",
     pid: 4,
     category: "accessories",
     quantity: 6,
@@ -44,7 +44,7 @@ const productList = [
     description:
       "A fleet of ships for you and your family. A separate ship for pets and other guests.",
     price: 500500,
-    image: "https://thumbs2.imgbox.com/b5/08/1BDH2uR1_t.jpg",
+    image: "https://www.flaticon.com/svg/static/icons/svg/3538/3538041.svg",
     pid: 5,
     category: "ships",
     quantity: 1,
@@ -54,7 +54,7 @@ const productList = [
     description:
       "A common Asteroid for your Home. Preferably store in the yard. Keep away from direct sunlight.",
     price: 199999,
-    image: "https://thumbs2.imgbox.com/e6/6b/ObxN91rr_t.jpg",
+    image: "https://www.flaticon.com/svg/static/icons/svg/3413/3413211.svg",
     pid: 6,
     category: "asteroids",
     quantity: 120,
@@ -64,7 +64,7 @@ const productList = [
     description:
       "A perfect gift for your loved ones. Makes for a great gift for occassions such as Valentines Day, Wedding Anniversaries, Birthdays etc",
     price: 29999,
-    image: "https://thumbs2.imgbox.com/b3/17/8nbqEGRf_t.jpg",
+    image: "https://www.flaticon.com/svg/static/icons/svg/2909/2909599.svg",
     pid: 7,
     category: "stars",
     quantity: 900,
@@ -73,7 +73,7 @@ const productList = [
     title: "Moon",
     description: "Your own personal Moon.",
     price: 599999,
-    image: "https://thumbs2.imgbox.com/6e/51/HAhw2K98_t.jpg",
+    image: "https://www.flaticon.com/svg/static/icons/svg/2909/2909605.svg",
     pid: 8,
     category: "moons",
     quantity: 1,
